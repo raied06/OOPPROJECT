dafd2e6b2c86660c176988a9358dae887e5db1da
raied06 <gigapower1070@gmail.com>
2026-05-01 16:10:16 +0500

    hello

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,21 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 int main() {
-	std::cout << "Hello world";
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Finally Works");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
